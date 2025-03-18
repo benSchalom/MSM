b09c06720ff799ec1f54565a731859d665bbf56c
ben <benschalom8@gmail.com>
2025-03-18 16:20:53 -0500

    Configuration Laravel pour MongoDb
    	modifié :         MSManagement/config/database.php

--- MSManagement/config/database.php
+++ MSManagement/config/database.php
@@ -97,6 +97,18 @@ return [
             'sslmode' => 'prefer',
         ],
 
+        'mongodb' => [
+            'driver' => 'mongodb',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', 27017),
+            'database' => env('DB_DATABASE', 'MSManagement'),
+            'username' => env('DB_USERNAME', ''),
+            'password' => env('DB_PASSWORD', ''),
+            'options' => [
+                'database' => env('DB_AUTHENTICATION_DATABASE', 'admin'),
+            ],
+        ],
+
         'sqlsrv' => [
             'driver' => 'sqlsrv',
             'url' => env('DB_URL'),
